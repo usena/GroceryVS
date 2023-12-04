3309a5d308327f2ff265fbc44f650e98cc653979
GooGaaGee <viriya.s@ligercambodia.org>
2023-12-04 16:16:54 +0700

    Update driverVS.py

--- driverVS.py
+++ driverVS.py
@@ -1,10 +1,9 @@
 from classmoduleVS import Place_OrderVS
+foodItems = dict()
 def orderVS():
-    foodItems={}
     while True:
-        numItems=int(input("How many items will you order today?: "))
+        numItems=int(input("How many items will you order today?:"))
         if numItems >= 1:
-            
             break
         else:
             print('Number of items must be at least 1.')
@@ -21,22 +20,23 @@ def orderVS():
         foodItems[itemName]=itemAmount
     return foodItems
 
+cost=list()
 def display_itemsVS(food):
-    cost = []
-    print("Here's a summary of  the items purchased: ")
-    for x,y in food:
-        item = Place_OrderVS()
-        item.__init__(x,y)
-        item.__PriceListVS()
-        item.calculatedCostVS()
-        print(f'Item: {item.__food} \n Amount ordered: {item.__amount} pounds \n Price per pound: {item.__price} \n Price of order: {item.__totalPrice}')
-        cost.append(item.__totalPrice)
+    print("\nHere's a summary of  the items purchased:")
+    print("**********\n")
+    for x,y in food.items():
+        item = Place_OrderVS(x,y)
+        item.__str__()
+        cost.append(item.getTotal())
+    return cost
 
 def totalCostVS(cost):
     cost = sum(cost)
-    return f'Total cost: ${cost}'
+    print(f'TOTAL COST: ${cost}')
 
 def groceryVS():
     orderVS()
     display_itemsVS(foodItems)
-    
+    totalCostVS(cost)
+
+groceryVS()
